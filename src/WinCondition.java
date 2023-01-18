24c8ec9711427d3dd62bcd9719ba8ee8eee28aa5
unknown <BT_3W2_32@BKTECH.LOCAL>
2023-01-18 09:25:34 -0500

    Initial commit

---
+++ src/WinCondition.java
@@ -0,0 +1,19 @@
+public class WinCondition {
+    private int[] winningSpaces;
+    private String hey;
+    private int winSpace;
+
+    public WinCondition(int one, int two, int three)
+    {
+
+
+    }
+
+
+
+
+    public int[] getWinningSpaces()
+    {
+        return winningSpaces;
+    }
+}
